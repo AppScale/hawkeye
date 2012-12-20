cd410c6efd94cb6ad002c12faf11755d7af3f8f9
Chris Bunch <shatterednirvana@gmail.com>
2012-12-20 11:56:44 -0800

    changed how we import json to be appengine-friendly

--- python-app/datastore.py
+++ python-app/datastore.py
@@ -1,4 +1,8 @@
-import json
+try:
+  import json
+except ImportError:
+  import simplejson as json
+
 import uuid
 import wsgiref
 from google.appengine.ext import webapp, db
--- python-app/ndb.py
+++ python-app/ndb.py
@@ -1,4 +1,8 @@
-import json
+try:
+  import json
+except ImportError:
+  import simplejson as json
+
 import wsgiref
 from google.appengine.ext import ndb, webapp
 import webapp2
