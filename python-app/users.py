9126b2241b8f024253e330bc302f0c7024cd3e7e
Hiranya Jayathilaka <hiranya911@gmail.com>
2012-12-26 09:49:17 -0800

    Adding users url handler

--- python-app/users.py
+++ python-app/users.py
@@ -14,10 +14,29 @@ class AuthorizedAccessHandler(webapp2.RequestHandler):
   def get(self):
     user = users.get_current_user()
     self.response.headers['Content-Type'] = "application/json"
-    self.response.out.write(json.dumps({ 'user' : user.nickname(), 'email' : user.email() }))
+    self.response.out.write(json.dumps(
+        { 'user' : user.nickname(), 'email' : user.email() }))
+
+class LoginURLHandler(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    self.response.headers['Content-Type'] = "application/json"
+    if user:
+      info = {
+        'type' : 'logout',
+        'url' : users.create_login_url("/python/users/")
+      }
+
+    else:
+      info = {
+        'type' : 'login',
+        'url' : users.create_logout_url("/python/users/")
+      }
+    self.response.out.write(json.dumps(info))
 
 application = webapp.WSGIApplication([
   ('/python/users/secure', AuthorizedAccessHandler),
+  ('/python/users/home', LoginURLHandler),
 ], debug=True)
 
 if __name__ == '__main__':
