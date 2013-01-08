01b8ee3e241252b36a924a896fab8773655bd350
Chris Bunch <shatterednirvana@gmail.com>
2013-01-08 10:04:07 -0800

    added pseudocode for xmpp routes

--- python-app/xmpp.py
+++ python-app/xmpp.py
@@ -21,22 +21,35 @@ class XmppHandler(webapp2.RequestHandler):
   so to aid in debugging, we use the Datastore to store
   the state of messages that have been sent or received.
   To ensure that the Datastore info is in a known state,
-  we add a third route to clean up old data.
+  we add a third route to clean up old data. The intended
+  state machine for XMPP message metadata is:
+  NOT EXISTENT -> MESSAGE SENT -> MESSAGE RECEIVED
   """
 
   def get(self):
     """Sends an XMPP from this app to itself.
     """
+    # send the message
+
+    # update our metadata
+
+    # write out the result, in json
     pass
 
   def post(self):
     """Receives an XMPP message previously sent to this app.
     """
+    # try to receive the message
+
+    # if no message, return failure
+
+    # if message, update our metadata and send success
     pass
 
   def delete(self):
     """Cleans up Datastore state concerning XMPP messages.
     """
+    # if the metadata exists, delete it
     pass
 
 application = webapp.WSGIApplication([
