7466bf98dde4af2c01e753d0d324fbe9453bc7d3
Jovan Chohan <jovan@appscale.com>
2013-02-04 19:56:34 -0800

    New hawkeye tests to make sure nginx locations are handled as regular expressions

--- test-suite/tests/secure_url_tests.py
+++ test-suite/tests/secure_url_tests.py
@@ -24,9 +24,33 @@ class AlwaysSecureTest(HawkeyeTestCase):
     response = self.http_get(redirect_url, prepend_lang=False, ssl=True)
     self.assertEquals(response.status, 200)
 
+class AlwaysSecureRegexTest(HawkeyeTestCase):
+  def run_hawkeye_test(self):
+    response = self.http_get('/secure/always/regex1/regex2')
+    self.assertEquals(response.status, 302)
+    self.assertTrue(response.headers.has_key('location'))
+    redirect_url = response.headers['location']
+    self.assertTrue(redirect_url.startswith('https:'))
+
+    response = self.http_get(redirect_url, prepend_lang=False, ssl=True)
+    self.assertEquals(response.status, 200)
+
+class NeverSecureRegexTest(HawkeyeTestCase):
+  def run_hawkeye_test(self):
+    response = self.http_get('/secure/never/regex1/regex2', ssl=True)
+    self.assertEquals(response.status, 302)
+    self.assertTrue(response.headers.has_key('location'))
+    redirect_url = response.headers['location']
+    self.assertTrue(redirect_url.startswith('http:'))
+
+    response = self.http_get(redirect_url, prepend_lang=False)
+    self.assertEquals(response.status, 200)
+
 def suite(lang):
   suite = HawkeyeTestSuite('Secure URL Test Suite', 'secure_url')
   if lang == 'python':
     suite.addTest(NeverSecureTest())
     suite.addTest(AlwaysSecureTest())
+    suite.addTest(NeverSecureRegexTest())
+    suite.addTest(AlwaysSecureRegexTest())
   return suite
