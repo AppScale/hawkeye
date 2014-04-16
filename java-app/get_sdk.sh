013e2e20dbee2b105a4b7f17ea41a6bfd7646211
graziano obertelli <graziano@eucalyptus.com>
2014-04-16 16:46:51 -0700

    Use a cached version of the sdk if available.

--- java-app/get_sdk.sh
+++ java-app/get_sdk.sh
@@ -1,5 +1,9 @@
 cd ../../
-wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.8.0.zip
+if [ -e ${HOME}/appengine-java-sdk-1.8.0.zip ]; then
+        cp ${HOME}/appengine-java-sdk-1.8.0.zip .
+else
+        wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.8.0.zip
+fi
 unzip appengine-java-sdk-1.8.0.zip
 rm appengine-java-sdk-1.8.0.zip
 cd -
