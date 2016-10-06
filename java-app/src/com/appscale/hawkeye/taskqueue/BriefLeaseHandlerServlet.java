d48ac17d58623f8c4115236768b7af0bb32220f3
Chris Donati <chris.donati@appscale.com>
2016-10-06 15:32:46 -0700

    Ensure tasks are only leased once per request

---
+++ java-app/src/com/appscale/hawkeye/taskqueue/BriefLeaseHandlerServlet.java
@@ -0,0 +1,45 @@
+package com.appscale.hawkeye.taskqueue;
+
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskHandle;
+import com.google.appengine.api.taskqueue.TaskOptions;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class BriefLeaseHandlerServlet extends HttpServlet {
+
+    private static final String HAWKEYEJAVA_PULL_QUEUE = "hawkeyejava-PullQueue-0";
+    private static final Queue queue = QueueFactory.getQueue(HAWKEYEJAVA_PULL_QUEUE);
+    private static final int SMALL_WAIT = 5000;  // A small interval to wait for a service.
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String payload = "hello world";
+        TaskOptions genericTaskParams = TaskOptions.Builder.withMethod(TaskOptions.Method.PULL).payload(payload);
+        queue.add(genericTaskParams);
+        queue.add(genericTaskParams);
+
+        try {
+            Thread.sleep(SMALL_WAIT);
+        } catch (InterruptedException e ) {
+            response.setStatus(500);
+            return;
+        }
+
+        // Only 2 tasks should be leased despite asking for 3.
+        List<TaskHandle> tasks = queue.leaseTasks(1, TimeUnit.NANOSECONDS, 3);
+        if (tasks.size() > 2) {
+            response.setStatus(500);
+            response.getWriter().write("Leased 3 tasks when only 2 were available.");
+        }
+    }
+
+}
