4c31e7e6a852d9e47dd2120cdb8061c42cde93bc
Chris Donati <chris.donati@appscale.com>
2017-03-21 21:07:34 -0700

    Move testing logic to client side

--- python27-app/logservice.py
+++ python27-app/logservice.py
@@ -1,74 +1,26 @@
-try:
-  import json
-except ImportError:
-  import simplejson as json
+import json
+import logging
+import webapp2
 
 from google.appengine.api import logservice
-from google.appengine.api.logservice import InvalidArgumentError
-from google.appengine.ext import webapp
-import webapp2
-import wsgiref
-import logging
+from itertools import islice
 
 
 class LogServiceHandler(webapp2.RequestHandler):
-
   def get(self):
-    logging.info("LogServiceHandler called")
-    dump = {}
-    log_message1 = "log message 1"
-    logservice.write(log_message1)
-    log_message2 = "log message 2"
-    logservice.write(log_message2)
-    dump['logs'] = [log_message1, log_message2]
-    self.response.headers['Content-Type'] = "application/json"
-    try:
-      fetched_logs = logservice.fetch()
-      dump['fetched_logs'] = fetched_logs
-      if len(fetched_logs) != 2:
-        dump['fetched'] = "incorrect count of fetched logs"
-        logging.info(json.dumps(dump))
-        self.response.set_status(404)
-      else:
-        request_ids = []
-        start_times = []
-        end_times = []
-        offsets = []
-        version_ids = []
-        for log in fetched_logs:
-          start_times.append(log.start_time)
-          end_times.append(log.end_time)
-          offsets.append(log.offset)
-          version_ids.append(log.version_id)
-          request_ids.append(log.request_id)
+    count = int(self.request.get('count'))
+    logs = logservice.fetch(include_app_logs=True)
+    entries = []
+    for entry in islice(logs, count):
+      entries.append([log.message for log in entry.app_logs])
+    json.dump(entries, self.response)
 
-      fetched_logs = logservice.fetch(request_ids=request_ids)
-      if len(fetched_logs) != 2:
-        dump['fetched_by_req_id'] = "incorrect count"
-        logging.info(json.dumps(dump))
-        self.response.set_status(404)
-      else:
-        got_all_req_ids = True
-        for log in fetched_logs:
-          if log.request_id not in request_ids:
-            got_all_req_ids = False
-        if not got_all_req_ids:
-          dump['got_all_req_ids'] = False
-          logging.info(json.dumps(dump))
-          self.response.set_status(404)
-        else:
-         self.response.set_status(200)
+  def post(self):
+    messages = self.request.get_all('message')
+    for message in messages:
+      logging.info(message)
 
-    except InvalidArgumentError:
-      logging.info(json.dumps(dump))
-      self.response.set_status(404)
-    except NameError:
-      logging.info(json.dumps(dump))
-      self.response.set_status(404)
 
-application = webapp.WSGIApplication([
-  ('/python/logserver', LogServiceHandler),
-], debug=True)
-
-if __name__ == '__main__':
-  wsgiref.handlers.CGIHandler().run(application)
+urls = [
+  ('/python/logserver', LogServiceHandler)
+]
--- test-suite/tests/logservice_tests.py
+++ test-suite/tests/logservice_tests.py
@@ -1,28 +1,27 @@
 import json
-import ssl
-import urllib2
-import urlparse
-from hawkeye_utils import HawkeyeTestCase, HawkeyeTestSuite
-import hawkeye_utils
+import urllib
 
-FILE_UPLOADS = {}
+from hawkeye_test_runner import HawkeyeTestSuite
+from hawkeye_utils import HawkeyeTestCase
 
-FILE1 = 'file1.txt'
-FILE2 = 'file2.txt'
-FILE3 = 'file3.txt'
-
-FILE1_DATA = 'FILE UPLOAD CONTENT'
-FILE2_DATA = 'AppScale Rocks!!!'
-FILE3_DATA = 'ASYNC FILE UPLOAD CONTENT'
 
 class FetchLogTest(HawkeyeTestCase):
   def run_hawkeye_test(self):
-    response = self.http_get('/logserver')
-    print(response.body)
+    requests = [['two logs are given', 'two are desired'],
+                ['this is the first request', 'so expect it last']]
+    for request in reversed(requests):
+      payload = urllib.urlencode({'message': request}, doseq=True)
+      self.http_post('/logserver', payload)
+
+    response = self.http_get('/logserver?count={}'.format(len(requests)))
     self.assertEquals(response.status, 200)
+    self.assertListEqual(requests, json.loads(response.payload))
 
 
 def suite(lang):
   suite = HawkeyeTestSuite('Logservice Test Suite', 'logservice')
-  suite.addTest(FetchLogTest())
+
+  if lang == 'python':
+    suite.addTest(FetchLogTest())
+
   return suite
