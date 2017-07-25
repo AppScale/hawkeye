8058c81825bcf4d6207ff9b03d0b5f77c6985f44
Chris Donati <chris.donati@appscale.com>
2017-07-25 10:34:46 -0700

    Use LogService to check if request completed

--- python27-app/logservice.py
+++ python27-app/logservice.py
@@ -1,12 +1,35 @@
 import json
 import logging
+import os
 import webapp2
 
 from google.appengine.api import logservice
 from itertools import islice
 
 
-class LogServiceHandler(webapp2.RequestHandler):
+class CheckFinishedHandler(webapp2.RequestHandler):
+  def get(self):
+    request_id = self.request.get('id')
+    logs = list(logservice.fetch(request_ids=[request_id]))
+
+    if not logs or not logs[0].finished:
+      finished = False
+    else:
+      finished = True
+
+    json.dump({'finished': finished}, self.response)
+
+
+class PutHandler(webapp2.RequestHandler):
+  def post(self):
+    messages = self.request.get_all('message')
+    for message in messages:
+      logging.info(message)
+
+    json.dump({'request_id': os.environ.get('REQUEST_LOG_ID')}, self.response)
+
+
+class QueryHandler(webapp2.RequestHandler):
   def get(self):
     count = int(self.request.get('count'))
     logs = logservice.fetch(include_app_logs=True)
@@ -15,12 +38,9 @@ class LogServiceHandler(webapp2.RequestHandler):
       entries.append([log.message for log in entry.app_logs])
     json.dump(entries, self.response)
 
-  def post(self):
-    messages = self.request.get_all('message')
-    for message in messages:
-      logging.info(message)
-
 
 urls = [
-  ('/python/logserver', LogServiceHandler)
+  ('/python/logserver/is_finished', CheckFinishedHandler),
+  ('/python/logserver/put', PutHandler),
+  ('/python/logserver/query', QueryHandler)
 ]
--- test-suite/tests/logservice_tests.py
+++ test-suite/tests/logservice_tests.py
@@ -4,15 +4,32 @@ import urllib
 from hawkeye_test_runner import DeprecatedHawkeyeTestCase, HawkeyeTestSuite
 
 
-class FetchLogTest(DeprecatedHawkeyeTestCase):
+class FinishedRequestTest(DeprecatedHawkeyeTestCase):
+  def run_hawkeye_test(self):
+    payload = urllib.urlencode({'message': 'foo'})
+    response = self.http_post('/logserver/put', payload)
+    request_id = json.loads(response.payload)['request_id']
+
+    # Ensure the request was marked as being finished.
+    response = self.http_get('/logserver/is_finished?id={}'.format(request_id))
+    self.assertEquals(response.status, 200)
+    self.assertTrue(json.loads(response.payload)['finished'])
+
+    # Make sure non-existent requests are marked as not finished.
+    response = self.http_get('/logserver/is_finished?id={}'.format('no-id'))
+    self.assertEqual(response.status, 200)
+    self.assertFalse(json.loads(response.payload)['finished'])
+
+
+class QueryLogsTest(DeprecatedHawkeyeTestCase):
   def run_hawkeye_test(self):
     requests = [['two logs are given', 'two are desired'],
                 ['this is the first request', 'so expect it last']]
     for request in reversed(requests):
       payload = urllib.urlencode({'message': request}, doseq=True)
-      self.http_post('/logserver', payload)
+      self.http_post('/logserver/put', payload)
 
-    response = self.http_get('/logserver?count={}'.format(len(requests)))
+    response = self.http_get('/logserver/query?count={}'.format(len(requests)))
     self.assertEquals(response.status, 200)
     self.assertListEqual(requests, json.loads(response.payload))
 
@@ -21,6 +38,7 @@ def suite(lang, app):
   suite = HawkeyeTestSuite('Logservice Test Suite', 'logservice')
 
   if lang == 'python':
-    suite.addTests(FetchLogTest.all_cases(app))
+    suite.addTests(FinishedRequestTest.all_cases(app))
+    suite.addTests(QueryLogsTest.all_cases(app))
 
   return suite
