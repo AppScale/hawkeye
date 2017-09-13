aa1bd094968849c4741e144f038d0cabef8f880b
Anton Leonov <jeanleonov@gmail.com>
2017-09-13 16:02:17 +0300

    Fixes to modules tests

--- test-suite/tests/modules_tests.py
+++ test-suite/tests/modules_tests.py
@@ -15,22 +15,22 @@ class TestVersionDetails(HawkeyeTestCase):
     response = self.app.get('/modules/versions-details', module='default')
     self.assertEquals(response.status_code, 200)
     actual = response.json()
-    self.assertEqual(actual['modules'], ['default', 'module-a'])
-    self.assertEqual(actual['current_module_versions'], ['1'])
-    self.assertEqual(actual['default_version'], '1')
+    self.assertItemsEqual(actual['modules'], ['default', 'module-a'])
+    self.assertEqual(actual['current_module_versions'], ['v1'])
+    self.assertEqual(actual['default_version'], 'v1')
     self.assertEqual(actual['current_module'], 'default')
-    self.assertEqual(actual['current_version'], '1')
+    self.assertEqual(actual['current_version'], 'v1')
     self.assertIsInstance(actual['current_instance_id'], basestring)
 
   def test_module_a(self):
     response = self.app.get('/modules/versions-details', module='module-a')
     self.assertEquals(response.status_code, 200)
     actual = response.json()
-    self.assertEqual(actual['modules'], ['default', 'module-a'])
-    self.assertEqual(actual['current_module_versions'], ['1'])
-    self.assertEqual(actual['default_version'], '1')
+    self.assertItemsEqual(actual['modules'], ['default', 'module-a'])
+    self.assertEqual(actual['current_module_versions'], ['v1'])
+    self.assertEqual(actual['default_version'], 'v1')
     self.assertEqual(actual['current_module'], 'module-a')
-    self.assertEqual(actual['current_version'], '1')
+    self.assertEqual(actual['current_version'], 'v1')
     self.assertIsInstance(actual['current_instance_id'], basestring)
 
 
@@ -52,7 +52,7 @@ class TestCreatingAndGettingEntity(HawkeyeTestCase):
     self.assertEquals(entity, {
       'id': entity_id,
       'created_at_module': 'default',
-      'created_at_version': '1'
+      'created_at_version': 'v1'
     })
 
     # Fetch this entity using module-a
@@ -62,7 +62,7 @@ class TestCreatingAndGettingEntity(HawkeyeTestCase):
     self.assertEquals(entity, {
       'id': entity_id,
       'created_at_module': 'default',
-      'created_at_version': '1',
+      'created_at_version': 'v1',
       'new_field': None
     })
 
@@ -79,7 +79,7 @@ class TestCreatingAndGettingEntity(HawkeyeTestCase):
     self.assertEquals(entity, {
       'id': entity_id,
       'created_at_module': 'module-a',
-      'created_at_version': '1'
+      'created_at_version': 'v1'
     })
 
     # Fetch this entity using module-a
@@ -89,7 +89,7 @@ class TestCreatingAndGettingEntity(HawkeyeTestCase):
     self.assertEquals(entity, {
       'id': entity_id,
       'created_at_module': 'module-a',
-      'created_at_version': '1',
+      'created_at_version': 'v1',
       'new_field': 'new'
     })
 
@@ -105,10 +105,6 @@ class TestTaskTargets(HawkeyeTestCase):
       ('queue-with-missed-target', str(uuid.uuid4())),
       ('queue-for-default', str(uuid.uuid4())),
       ('queue-for-module-a', str(uuid.uuid4())),
-      ('deferred-default-queue', str(uuid.uuid4())),
-      ('deferred-queue-with-missed-target', str(uuid.uuid4())),
-      ('deferred-queue-for-default', str(uuid.uuid4())),
-      ('deferred-queue-for-module-a', str(uuid.uuid4())),
     ])
 
     # Push entity-creation tasks to default queue
@@ -126,21 +122,6 @@ class TestTaskTargets(HawkeyeTestCase):
                  params={'id': self.entity_ids['queue-for-module-a'],
                          'queue': 'queue-for-module-a'})
 
-    # Defer entity-creation tasks to default queue
-    self.app.get('/modules/defer-task',
-                 params={'id': self.entity_ids['deferred-default-queue']})
-
-    # Defer entity-creation tasks to other queues
-    self.app.get('/modules/defer-task',
-                 params={'id': self.entity_ids['deferred-queue-with-missed-target'],
-                         'queue': 'queue-with-missed-target'})
-    self.app.get('/modules/defer-task',
-                 params={'id': self.entity_ids['deferred-queue-for-default'],
-                         'queue': 'queue-for-default'})
-    self.app.get('/modules/defer-task',
-                 params={'id': self.entity_ids['deferred-queue-for-module-a'],
-                         'queue': 'queue-for-module-a'})
-
     # Target is set explicitly only in Python apps
     if self.app.language == 'python':
 
@@ -148,6 +129,10 @@ class TestTaskTargets(HawkeyeTestCase):
       self.entity_ids['target-module-a'] = str(uuid.uuid4())
       self.entity_ids['deferred-target-default'] = str(uuid.uuid4())
       self.entity_ids['deferred-target-module-a'] = str(uuid.uuid4())
+      self.entity_ids['deferred-default-queue'] = str(uuid.uuid4())
+      self.entity_ids['deferred-queue-with-missed-target'] = str(uuid.uuid4())
+      self.entity_ids['deferred-queue-for-default'] = str(uuid.uuid4())
+      self.entity_ids['deferred-queue-for-module-a'] = str(uuid.uuid4())
 
       # Push entity-creation tasks with specified targets
       self.app.get('/modules/add-task',
@@ -165,6 +150,21 @@ class TestTaskTargets(HawkeyeTestCase):
                    params={'id': self.entity_ids['deferred-target-module-a'],
                            'target': 'module-a'})
 
+      # Defer entity-creation tasks to default queue
+      self.app.get('/modules/defer-task',
+                   params={'id': self.entity_ids['deferred-default-queue']})
+
+      # Defer entity-creation tasks to other queues
+      self.app.get('/modules/defer-task',
+                   params={'id': self.entity_ids['deferred-queue-with-missed-target'],
+                           'queue': 'queue-with-missed-target'})
+      self.app.get('/modules/defer-task',
+                   params={'id': self.entity_ids['deferred-queue-for-default'],
+                           'queue': 'queue-for-default'})
+      self.app.get('/modules/defer-task',
+                   params={'id': self.entity_ids['deferred-queue-for-module-a'],
+                           'queue': 'queue-for-module-a'})
+
     time.sleep(2)
 
   def test_task_targets(self):
@@ -177,16 +177,16 @@ class TestTaskTargets(HawkeyeTestCase):
       'queue-with-missed-target': 'default',
       'queue-for-default': 'default',
       'queue-for-module-a': 'module-a',
-      'deferred-default-queue': 'default',
-      'deferred-queue-with-missed-target': 'default',
-      'deferred-queue-for-default': 'default',
-      'deferred-queue-for-module-a': 'module-a',
     }
     if self.app.language == 'python':
       expectation['target-default'] = 'default'
       expectation['target-module-a'] = 'module-a'
       expectation['deferred-target-default'] = 'default'
       expectation['deferred-target-module-a'] = 'module-a'
+      expectation['deferred-default-queue'] = 'default'
+      expectation['deferred-queue-with-missed-target'] = 'default'
+      expectation['deferred-queue-for-default'] = 'default'
+      expectation['deferred-queue-for-module-a'] = 'module-a'
 
     miss_match = []
     for entity_alias, entity in entities:
