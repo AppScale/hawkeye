94ac5a030b964b93aa1c9ffb882c89a301089b97
Anton Leonov <jeanleonov@gmail.com>
2017-09-15 13:35:50 +0300

    Service API was introduced to main module of Java app

---
+++ java-app/module-main/src/com/appscale/hawkeye/modules/GetVersionDetailsHandlerServlet.java
@@ -0,0 +1,32 @@
+package com.appscale.hawkeye.modules;
+
+import com.appscale.hawkeye.JSONUtils;
+
+import com.google.appengine.api.labs.modules.ModulesService;
+import com.google.appengine.api.labs.modules.ModulesServiceFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetVersionDetailsHandlerServlet extends HttpServlet {
+
+    ModulesService modulesApi = ModulesServiceFactory.getModulesService();
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        Map<String,Object> map = new HashMap<String, Object>();
+        String currentModuleName = modulesApi.getCurrentModule();
+        map.put("modules", modulesApi.getModules());
+        map.put("current_module_versions", modulesApi.getVersions(currentModuleName));
+        map.put("default_version", modulesApi.getDefaultVersion(currentModuleName));
+        map.put("current_module", currentModuleName);
+        map.put("current_version", modulesApi.getCurrentVersion());
+        map.put("current_instance_id", modulesApi.getCurrentInstanceId());
+        JSONUtils.serialize(map, response);
+    }
+}
