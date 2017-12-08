7f3c98bcaf3efbd77ffd006c45d69b0f11db9f84
whoarethebritons <eden.akins@gmail.com>
2017-12-07 20:32:03 -0800

    fix typo

--- test-suite/tests/logservice_tests.py
+++ test-suite/tests/logservice_tests.py
@@ -20,20 +20,19 @@ class AutoFlushLogTest(DeprecatedHawkeyeTestCase):
   """ This test is to make sure that wee don't get duplicate log entries from
   autoflushing."""
   def run_hawkeye_test(self):
-    def run_hawkeye_test(self):
-      # The log threshold for flushing is 50 so we make it 51 so that it will
-      # autoflush.
-      log_lines = 51
-      request = ["request line {}".format(i) for i in range(log_lines)]
+    # The log threshold for flushing is 50 so we make it 51 so that it will
+    # autoflush.
+    log_lines = 51
+    request = ["request line {}".format(i) for i in range(log_lines)]
 
-      payload = urllib.urlencode({'message': request}, doseq=True)
-      self.http_post('/logserver', payload)
+    payload = urllib.urlencode({'message': request}, doseq=True)
+    self.http_post('/logserver', payload)
 
-      response = self.http_get('/logserver?count=1')
-      self.assertEquals(response.status, 200)
-      results = json.loads(response.payload)
+    response = self.http_get('/logserver?count=1')
+    self.assertEquals(response.status, 200)
+    results = json.loads(response.payload)
 
-      self.assertListEqual(results[0], request)
+    self.assertListEqual(results[0], request)
 
 
 def suite(lang, app):
