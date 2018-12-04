3b6263ae8005689a3502513aaceaaaa258a02d28
Steven Graham <1858109+scragraham@users.noreply.github.com>
2018-12-04 16:22:31 -0600

    - make sure to cd back into the previous directory on error conditions.  - properly cleanup sdk zip file on errors.

--- java-app/get_sdk.sh
+++ java-app/get_sdk.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 JAVA_SDK_VERSION=1.8.4
 JAVA_SDK_ZIP=appengine-java-sdk-${JAVA_SDK_VERSION}.zip
 SDK_URL="http://s3.amazonaws.com/appscale-build/${JAVA_SDK_ZIP}"
@@ -7,17 +9,20 @@ if [ -e ${HOME}/${JAVA_SDK_ZIP} ]; then
         cp ${HOME}/${JAVA_SDK_ZIP} .
 else
         if ! wget ${SDK_URL}; then
-		echo "Failed to retrieve SDK from ${SDK_URL}, exiting script"
-		exit 1
-	fi
+                echo "Failed to retrieve SDK from ${SDK_URL}, exiting script"
+                cd -
+                exit 1
+        fi
 fi
 
 echo "Extracting ${JAVA_SDK_ZIP} to ${PWD}"
 
 if ! unzip -q ${JAVA_SDK_ZIP}; then
-	echo "Failed to unzip SDK correctly, please see errors above"
-	exit 1
+        echo "Failed to unzip SDK correctly, please see errors above"
+        rm -f ${JAVA_SDK_ZIP}
+        cd -
+        exit 1
 fi
-rm -f ${JAVA_SDK_ZIP}
 
+rm -f ${JAVA_SDK_ZIP}
 cd -
