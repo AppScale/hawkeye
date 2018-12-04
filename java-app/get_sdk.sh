69ddd0272c077bb037c51f553d5f8108c1eceea4
Steven Graham <1858109+scragraham@users.noreply.github.com>
2018-12-04 14:56:10 -0600

    Minor adjustments to get_sdk.sh script  - change url to an S3 bucket since google no longer stores that sdk  - added error checking to wget command an unzip commands  - added more info as to where the sdk is being extracted to.

--- java-app/get_sdk.sh
+++ java-app/get_sdk.sh
@@ -1,15 +1,23 @@
 JAVA_SDK_VERSION=1.8.4
 JAVA_SDK_ZIP=appengine-java-sdk-${JAVA_SDK_VERSION}.zip
+SDK_URL="http://s3.amazonaws.com/appscale-build/${JAVA_SDK_ZIP}"
 
 cd ../../
 if [ -e ${HOME}/${JAVA_SDK_ZIP} ]; then
         cp ${HOME}/${JAVA_SDK_ZIP} .
 else
-        wget http://googleappengine.googlecode.com/files/${JAVA_SDK_ZIP}
+        if ! wget ${SDK_URL}; then
+		echo "Failed to retrieve SDK from ${SDK_URL}, exiting script"
+		exit 1
+	fi
 fi
 
-echo "Extracting ${JAVA_SDK_ZIP}"
-unzip -q ${JAVA_SDK_ZIP}
-rm ${JAVA_SDK_ZIP}
+echo "Extracting ${JAVA_SDK_ZIP} to ${PWD}"
+
+if ! unzip -q ${JAVA_SDK_ZIP}; then
+	echo "Failed to unzip SDK correctly, please see errors above"
+	exit 1
+fi
+rm -f ${JAVA_SDK_ZIP}
 
 cd -
